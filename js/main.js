f354e21e729570c2101813020570a695b057fc3b
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-20 15:39:30 +0100

    aggiunto ReadMe in file js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// READ ME
+
+// - inserisco la modalità "use strict" per poter visualizzare
+// da console Inspector eventuali incongruenze di compilazione JS.
+
+// - inserisci variabili Nome, Cognome, Colore Preferito.
+
+// - inserisci prompt per chiedere all'utente le variabili
+// precedentemente create.
+
+// - lancia il log.console per la stampa del risultato Finale.
+
+
+'use strict';
+
+
