e36a2142aea908e8d267dc8fa444680c7258bcf0
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-20 17:44:05 +0100

    aggiustamento commenti file JS

--- js/main.js
+++ js/main.js
@@ -1,30 +1,44 @@
-// READ ME
+// --------------------------------------------------------
+// READ ME file JS
 
-// - inserisco la modalità "use strict" per poter visualizzare
-// da console Inspector eventuali incongruenze di compilazione JS.
+// - inserisco l'istruzione JS' "use strict" per poter visualizzare
+//  da console Inspector eventuali incongruenze di compilazione JS.
 
 // - inserisci variabili Nome, Cognome, Colore Preferito.
 
-// - inserisci prompt per chiedere all'utente le variabili
-// precedentemente create.
+// - inserisci prompt per chiedere all'utente, all'apertura del sito,
+//  le variabili precedentemente create
 
-// - lancia il log.console per la stampa del risultato Finale.
+// - lancia il console.log per la stampa del risultato finale.
+// -------------------------------------------------------------
 
 
 'use strict';
 
-// const firstName = prompt ('Digita il tuo nome');
-// const lastName = prompt('Digita il tuo cognome');
-// const favColor = prompt('Digita il tuo colore preferito');
+// Crea variabili di partenza con valore da inserire a richiesta
+const firstName = prompt ('PER FAVORE INSERISCI IL TUO NOME!!!');
+const lastName = prompt('NON FARMI ARRABBIARE E INSERISCI IL TUO COGNOME!!!');
+const favColor = prompt('SE VUOI SOPRAVVIVERE DIGITA IL TUO COLORE PREFERITO!!!');
 
+
+// Scrivi il valore della variabile a video nella pagina web,
+// in un campo collegato all'id dell'HTML
 document.getElementById('first_name').innerHTML= firstName;
 document.getElementById('last_name').innerHTML= lastName;
 document.getElementById('fav_color').innerHTML= favColor;
 
+
+// Crea variabile che concateni le 3 variabili precedentemente 
+// inizializzate con l'aggiunta della scritta '89'
 const userPassword = firstName + lastName + favColor + '89';
 
+// Scrivi il valore della nuova variabile password nella pagina web, 
+// in un campo collegato all'id dell'HTML
 document.getElementById('user_password').innerHTML= userPassword;
 
+
+// Stampa sulla console Inspector della pagina web il contenuto della
+// variabile userPassword
 console.log(userPassword);
 
 
